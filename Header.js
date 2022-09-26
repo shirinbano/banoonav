9460ea48878ad8b3a62e5d7c8239f9d0eda59867
shirin banoo <shirinkhorasanian@yahoo.com>
2022-09-26 15:19:14 +0300

    1.0.4

--- Header.js
+++ Header.js
@@ -7,7 +7,7 @@ import "./Styles/custom.scss"
 
 const Header = ({ headClass, leftTxt, rightTxt, btnClass }) => {
     return (
-        <div className={`${headClass ? headClass : 'bg-lightgrey position-fixed w-100 headerHeight py-3'}`}>
+        <div className={`${headClass ? headClass : 'bg-lightgrey position-fixed w-100 headerHeight py-2'}`}>
             <div className="container">
                 <div className="d-flex justify-content-between pt-2">
                     <div className="col-6">
